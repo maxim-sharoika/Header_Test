0a377de297781124caab8154a3edddd4f33b47be
Maksim Sharoika <maxim.sharoika@rbe.sk.ca>
2020-02-07 13:51:56 -0600

    Header_Test.py

---
+++ Header_Test.py
@@ -0,0 +1,8 @@
+# Course: CS 30
+# Period: 4
+# Date created: 07/02/20
+# Date last modified: 07/02/20
+# Name: Maksim Sharoika
+# Description: Test header program
+
+print("Header Test - Affirmative")
